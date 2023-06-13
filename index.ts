15323ac887628144b360da8e96ba999890c57a3d
Domai <github@domai-tb.com>
2023-06-13 11:22:26 +0200

    change api paths to fix wrong card output

--- index.ts
+++ index.ts
@@ -1,17 +1,18 @@
 import express from 'express'
-import api from './api'
+import api from './api/profile'
 import history from './api/history'
 import topLangs from './api/top-langs'
+import profile from './api/profile'
 
 const app = express()
 
 
-app.use('/api', api)
-app.use('/api/history', history)
-app.use('/api/top-langs', topLangs)
+app.use('/profile', profile)
+app.use('/history', history)
+app.use('/toplang', topLangs)
 
 app.get('/', (_, res) => {
-	res.redirect(301, 'https://github.com/aviortheking/codestats-readme')
+	res.redirect(301, 'https://github.com/domai-tb/codestats-readme')
 })
 
 app.listen(3000, () => {
