72f7e298b2ecb526d65b237357cdefb0b2a31ffe
Domai <github@domai-tb.com>
2023-09-24 15:26:57 +0200

    debug action

--- action.ts
+++ action.ts
@@ -18,6 +18,7 @@ async function main() {
     const username = core.getInput("username");
 
     // Fetch Code::Stats API
+    console.log(`Fetch account data: codestats.net/users/${username}`);
     const profile = await fetchProfile(username);
     const toplang = await fetchTopLanguages(username);
     const history = await fetchHistory(
@@ -28,19 +29,19 @@ async function main() {
     // Generate Profile Summary Card
     const profilecard = ReactDOMServer.renderToStaticMarkup(
       new ProfileCard(profile.username, profile.xp, profile.recentXp, {
-        hide: parseArray(core.getInput("profile_card_hide_lines")),
-        show_icons: core.getBooleanInput("profile_card_show_icons"),
-        hide_rank: core.getBooleanInput("profile_card_hide_rank"),
-        line_height: parseNumber(core.getInput("profile_card_line_height")),
-        title: core.getInput("profile_card_title")
-          ? core.getInput("profile_card_title")
-          : `Code::Stats of ${username}`,
-        title_color: core.getInput("common_title_color"),
-        icon_color: core.getInput("common_icon_color"),
-        text_color: core.getInput("common_text_color"),
-        bg_color: core.getInput("common_bg_color"),
-        hide_title: core.getBooleanInput("common_hide_title"),
-        hide_border: core.getBooleanInput("common_hide_border"),
+        // hide: parseArray(core.getInput("profile_card_hide_lines")),
+        // show_icons: core.getBooleanInput("profile_card_show_icons"),
+        // hide_rank: core.getBooleanInput("profile_card_hide_rank"),
+        // line_height: parseNumber(core.getInput("profile_card_line_height")),
+        // title: core.getInput("profile_card_title")
+        //   ? core.getInput("profile_card_title")
+        //   : `Code::Stats of ${username}`,
+        // title_color: core.getInput("common_title_color"),
+        // icon_color: core.getInput("common_icon_color"),
+        // text_color: core.getInput("common_text_color"),
+        // bg_color: core.getInput("common_bg_color"),
+        // hide_title: core.getBooleanInput("common_hide_title"),
+        // hide_border: core.getBooleanInput("common_hide_border"),
         theme:
           core.getInput("theme") in themes
             ? (core.getInput("theme") as keyof typeof themes)
@@ -54,22 +55,22 @@ async function main() {
     // Generate Top Languages Card
     const toplangcard = ReactDOMServer.renderToStaticMarkup(
       new TopLanguagesCard(username, toplang.langs, {
-        hide: parseArray(core.getInput("common_hide_languages")),
-        language_count: parseNumber(
-          core.getInput("toplangs_card_language_count")
-        ),
-        card_width: 500,
-        layout: core.getBooleanInput("toplangs_card_compact_layout")
-          ? "compact"
-          : undefined,
-        title: core.getInput("toplangs_card_title")
-          ? core.getInput("toplangs_card_title")
-          : `Code::Stats of ${username}`,
-        title_color: core.getInput("common_title_color"),
-        text_color: core.getInput("common_text_color"),
-        bg_color: core.getInput("common_bg_color"),
-        hide_title: core.getBooleanInput("common_hide_title"),
-        hide_border: core.getBooleanInput("common_hide_border"),
+        // hide: parseArray(core.getInput("common_hide_languages")),
+        // language_count: parseNumber(
+        //   core.getInput("toplangs_card_language_count")
+        // ),
+        // card_width: 500,
+        // layout: core.getBooleanInput("toplangs_card_compact_layout")
+        //   ? "compact"
+        //   : undefined,
+        // title: core.getInput("toplangs_card_title")
+        //   ? core.getInput("toplangs_card_title")
+        //   : `Code::Stats of ${username}`,
+        // title_color: core.getInput("common_title_color"),
+        // text_color: core.getInput("common_text_color"),
+        // bg_color: core.getInput("common_bg_color"),
+        // hide_title: core.getBooleanInput("common_hide_title"),
+        // hide_border: core.getBooleanInput("common_hide_border"),
         theme:
           core.getInput("theme") in themes
             ? (core.getInput("theme") as keyof typeof themes)
@@ -83,22 +84,22 @@ async function main() {
     // Generate History Card
     const historycard = ReactDOMServer.renderToStaticMarkup(
       new HistoryCard(username, history, {
-        hide: parseArray(core.getInput("common_hide_languages")),
-        language_count: parseNumber(
-          core.getInput("history_card_language_count")
-        ),
-        hide_legend: core.getBooleanInput("history_card_hide_legend"),
-        reverse_order: core.getBooleanInput("history_card_reverse_order"),
-        width: 500,
-        height: 300,
-        title_color: core.getInput("common_title_color"),
-        text_color: core.getInput("common_text_color"),
-        bg_color: core.getInput("common_bg_color"),
-        layout: core.getBooleanInput("history_card_horizontal_layout")
-          ? "horizontal"
-          : undefined,
-        hide_title: core.getBooleanInput("common_hide_title"),
-        hide_border: core.getBooleanInput("common_hide_border"),
+        // hide: parseArray(core.getInput("common_hide_languages")),
+        // language_count: parseNumber(
+        //   core.getInput("history_card_language_count")
+        // ),
+        // hide_legend: core.getBooleanInput("history_card_hide_legend"),
+        // reverse_order: core.getBooleanInput("history_card_reverse_order"),
+        // width: 500,
+        // height: 300,
+        // title_color: core.getInput("common_title_color"),
+        // text_color: core.getInput("common_text_color"),
+        // bg_color: core.getInput("common_bg_color"),
+        // layout: core.getBooleanInput("history_card_horizontal_layout")
+        //   ? "horizontal"
+        //   : undefined,
+        // hide_title: core.getBooleanInput("common_hide_title"),
+        // hide_border: core.getBooleanInput("common_hide_border"),
         theme:
           core.getInput("theme") in themes
             ? (core.getInput("theme") as keyof typeof themes)
